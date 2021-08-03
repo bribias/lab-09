6754c4759027b29f6e22d676651de3784182038d
Brianna Bias <brianna.bias@gmail.com>
2021-08-03 01:16:56 -0700

    post test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,10 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('creates a character via POST', async () => {
+    const character = { name: 'Katsuki Bakgugo', quirk: 'explosion' };
+    const res = await (await request(app).post('/api/character')).setEncoding(character);
+
+    expect(res.body).toEqual({ id: '1', ...character });
+  });
 });
